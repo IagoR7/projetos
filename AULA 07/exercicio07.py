42de60a2d23fc83667596b5d1bee0ffa93182326
IagoR7 <iago85210@gmail.com>
2024-09-04 21:16:22 -0300

    Update exercicio07.py

--- AULA 07/exercicio07.py
+++ AULA 07/exercicio07.py
@@ -4,23 +4,24 @@
 
 negativos = []
 positivo = []
-soma_positivos = 0
+
+
 
 print("Insira 10 números:")
-for _ in range(10):
-    numero = float(input())  # Recebe um número e o converte para float
+for i in range(10):
+    numero = float(input(f"digite o valor {i+1}: "))  # Recebe um número e o converte para float
     if numero < 0:
         negativos.append(numero)  # Adiciona número à lista de negativos
     elif numero > 0:
         positivo.append(numero)
-        soma_positivos += numero  # Soma os números positivos
+       
 
-
+soma = sum(positivo)
 qtd_negativos = len(negativos)
 qtd_positivos = len(positivo)
 
 print(f"Números negativos: {negativos}")
 print(f"Quantidade de negativos: {qtd_negativos}")
-print(f"Soma dos números positivos: {soma_positivos}")
+print(f"Soma dos números positivos: {soma}")
 print(f'numeros positivos{positivo}')
 print(f'quantidade de numeros positivos {qtd_positivos}')
